4f60099ff89c4874aa914efa548a5e58ea2224e6
Aaron Stanek <amr.stanek@gmail.com>
2022-07-18 16:31:10 -0700

    wrote makeVirtualAutoPath

--- src/virtualPath.ts
+++ src/virtualPath.ts
@@ -2,6 +2,10 @@ export function makeVirtualPath(path: string): string {
     return 'virtual\x00' + path;
 }
 
+export function makeVirtualAutoPath(path: string): string {
+    return makeVirtualPath('auto\x00' + path);
+}
+
 export function pathIsVirtual(path: string): boolean {
     return (path.slice(0,8) === 'virtual\x00');
 }
