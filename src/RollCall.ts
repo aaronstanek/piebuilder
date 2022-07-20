69a5a0d0832c7c79eb64b6c206599aea62606392
Aaron Stanek <amr.stanek@gmail.com>
2022-07-19 17:09:31 -0700

    added RollCall class

---
+++ src/RollCall.ts
@@ -0,0 +1,30 @@
+type RollCallResult = 'same' | 'cached' | 'built'
+
+interface RollCallDictionary {
+    [index: string]: RollCallResult;
+}
+
+class RollCall {
+    _paths: RollCallDictionary;
+    constructor() {
+        this._paths = {};
+    }
+    same(path: string) {
+        if (!(path in this._paths)) {
+            this._paths[path] = 'same';
+        }
+    }
+    cached(path: string) {
+        if (path in this._paths) {
+            if (this._paths[path] === 'same') {
+                this._paths[path] = 'cached';
+            }
+        }
+        else {
+            this._paths[path] = 'cached';
+        }
+    }
+    built(path: string) {
+        this._paths[path] = 'built';
+    }
+}
